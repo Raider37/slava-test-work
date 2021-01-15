da586dc14704e2281afbf56740e7edb875746eed
Raider37 <56130875+Raider37@users.noreply.github.com>
2021-01-15 20:39:28 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,3 +5,29 @@ let burger = document.getElementById('toggle');
 burger.onclick = function() {
   open.classList.toggle('open');
 };
+
+//////////////////////////////////
+
+
+/*let onee = document.getElementById('product');
+let twoo = document.getElementById('formInfo');
+
+onee.onclick = function() {
+  twoo.classList.add('active');
+};*/
+
+
+$('.shop-products-item').click(function(){
+  $('.open-info').addClass('active');
+})
+$('.close').click(function(){
+  $('.open-info').removeClass('active');
+})
+
+$('.header-nav-btn').click(function(){
+  $('.popup').toggleClass('active-popup');
+})
+$('.main').click(function(){
+  $('.popup').removeClass('active-popup');
+})
+
